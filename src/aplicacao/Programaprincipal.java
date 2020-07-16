7aa9068a11e601bbcbc9456ff9e86833faf1d234
Raul Silva <rts_raul_tomaz@hotmail.com>
2020-07-16 18:50:02 -0300

    Primeira classe: Posicao

--- src/aplicacao/Programaprincipal.java
+++ src/aplicacao/Programaprincipal.java
@@ -1,9 +1,13 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class Programaprincipal {
 
 	public static void main(String[] args) {
-		System.out.println("ola mundo");
+		
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 
 	}
 
